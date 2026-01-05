fd60bd94284cd1b02b8bca899433860b90cc0cf8
rlarhkdwn <rlarhkdwn0524@gmail.com>
2026-01-05 13:43:30 +0900

    2026-01-05

---
+++ www/src/test/java/com/StudyLink/www/WwwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StudyLink.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
